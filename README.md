e624e3f37613242a64f71a49da5319fd54bc7c33
christ.hg <christ.hg@gmail.com>
2019-08-19 11:31:13 +0800

    updated

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # docker-keepalived
 使用keepalived監測nginx單點故障達到ha高可用
+
+## clone
+#git clone to /root
+
+## docker-compose
+#cd /root/docker-keepalived  
+#docker-compose up -d  
+
+## cron to check nginx
+#vi /etc/crontab
+* * * * * root /root/docker-keepalived/check_nginx_service.sh 2>/dev/null 1>&
+
+## check_nginx_service.sh
++ the shell is to check nginx pid,if not exist then kill docker-keepalived
--- check_nginx_service.sh
+++ check_nginx_service.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-#在實體机上設定cron,每2秒檢測一次
+#在實體机上設定cron,每5秒檢測一次
 
 #判斷docker-nginx的nginx.pid是否存在,用來監看nginx服務是否啟動
 #因為實體机可能會啟動多個nginx服務,為了檢測是哪個nginx容器,因此
 #nginx容器必須掛載-v ./run/nginx.pid:/run/nginx.pid
 #獲取nginx.pid用以分別不同的nginx服務
+#----------------------------------------------------------------------------------
 
-file="/root/docker-nginx/run/nginx.pid"
-if [ -f "$file" ];then
-      #echo "nginx is running..."  #表示nginx服務啟動中
-   else
-      echo "killall keepalived..."  #表示nginx服務失敗...stop or kill keepalived容器
-      docker stop keepalived  # docker rm -f keepalived
+nginx_pid="/root/docker-nginx/run/nginx.pid" # 監看的nginx服務的pid
+keepalived_pid="/root/docker-keepalived/run/keepalived.pid" # 自身keepalived服務pid
+step=5 #间隔的秒数，不能大于60  
+
+if [ -f "$keepalived_pid" ];then
+   for (( i = 0; i < 60; i=(i+step) ));do  
+     if [ ! -f "$nginx_pid" ];then
+         #echo "killall keepalived..."  #表示nginx服務失敗...stop or kill keepalived容器
+         docker stop keepalived         # docker rm -f keepalived
+     else
+        echo "ngix is running..."
+     fi
+     sleep $step  
+   done  
 fi
 
+echo "keepalived is dead..."
+exit 0 
